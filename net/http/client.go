bff6815eabda1eb98901068b03cbed91a1cf00ab
马良红 <malh@enveesoft.com>
2017-05-12 10:36:32 +0800

    http/client.go Get PostJSON

---
+++ net/http/client.go
@@ -0,0 +1,38 @@
+package http
+
+import (
+	"bytes"
+	"io/ioutil"
+	nhttp "net/http"
+)
+
+func Get(url string) ([]byte, error) {
+	resp, err := nhttp.Get(url)
+	if err != nil {
+		return make([]byte, 0), nil
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return body, err
+}
+
+func PostJSON(url string, data []byte) ([]byte, error) {
+	req, err := nhttp.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return make([]byte, 0), nil
+	}
+
+	req.Header.Set("X-Custom-Header", "val")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &nhttp.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return make([]byte, 0), nil
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body, err
+}
