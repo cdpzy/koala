8a29bc4f3ee852efaeac5da8db268a763314b2e5
马良红 <malh@enveesoft.com>
2017-06-28 09:45:21 +0800

    filter.go

---
+++ helper/filter.go
@@ -0,0 +1,15 @@
+package helper
+
+import "unicode/utf8"
+
+// FilterEmoji 过滤emoji表情
+func FilterEmoji(content string) string {
+	new_content := ""
+	for _, value := range content {
+		_, size := utf8.DecodeRuneInString(string(value))
+		if size <= 3 {
+			new_content += string(value)
+		}
+	}
+	return new_content
+}
