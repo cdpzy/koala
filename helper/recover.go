c17625dfbd8e1ebeb41b13eba65d280baabfe85a
马良红 <malh@enveesoft.com>
2017-06-29 18:06:53 +0800

    panic

--- helper/recover.go
+++ helper/recover.go
@@ -24,3 +24,22 @@ func RecoverStack(extras ...interface{}) {
 		}
 	}
 }
+
+func RecoverStackPanic(extras ...interface{}) {
+	if x := recover(); x != nil {
+		log.Error(x)
+		i := 0
+		funcName, file, line, ok := runtime.Caller(i)
+		for ok {
+			log.Errorf("frame %v:[func:%v,file:%v,line:%v]\n", i, runtime.FuncForPC(funcName).Name(), file, line)
+			i++
+			funcName, file, line, ok = runtime.Caller(i)
+		}
+
+		for k := range extras {
+			log.Errorf("EXRAS#%v DATA:%v\n", k, spew.Sdump(extras[k]))
+		}
+
+		panic(x)
+	}
+}
